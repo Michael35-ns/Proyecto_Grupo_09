6ffae31cbcce91313d28fde9835663114aad586d
Cristopher <crisnuva1427@gmail.com>
2024-03-20 17:08:43 -0600

    Avance

---
+++ proyectoHealthyMind/src/main/java/com/proyectoHealthyMind/controller/SuicidioController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.proyectoHealthyMind.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/enfermedad")
+public class SuicidioController {
+    
+    @GetMapping("/suicidio")
+    public String page(Model model) {
+        model.addAttribute("attribute", "value");
+        return "/enfermedad/suicidio";
+    }
+}
