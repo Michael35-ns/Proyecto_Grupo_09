5ec7cf26dd70f69203e49d434122c5ac41e4feb5
Cristopher <crisnuva1427@gmail.com>
2024-03-22 11:42:13 -0600

    Se diseñó la vista depresion y se le dió formato al index, se intentó hacer las consultas, pero no pude

--- proyectoHealthyMind/src/main/java/com/proyectoHealthyMind/dao/UsuarioDao.java
+++
@@ -1,8 +0,0 @@
-package com.proyectoHealthyMind.dao;
-
-import com.proyectoHealthyMind.domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioDao extends JpaRepository<Usuario, Long> {
-    
-}
